324140390c87eb92a5b5b15a07d4f354b063532e
Johannes Seitz <johannes.seitz@namics.com>
2012-05-16 20:17:09 +0200

    static files

--- coderetreat/base/urls.py
+++ coderetreat/base/urls.py
@@ -2,6 +2,7 @@
 
 from django.conf.urls import patterns, include, url
 from django.views.generic.base import TemplateView
+from coderetreat import settings
 
 urlpatterns = patterns('',
     url(r"^$", TemplateView.as_view(template_name="home.html"),
@@ -10,4 +11,8 @@ urlpatterns = patterns('',
         name="learnmore"),
     url(r"^imprint", TemplateView.as_view(template_name="imprint.html"),
             name="imprint"),
+)
+
+urlpatterns += patterns('',
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
--- coderetreat/urls.py
+++ coderetreat/urls.py
@@ -1,13 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import patterns, include, url
-from coderetreat import settings
 
 urlpatterns = patterns('',
     url(r"", include("coderetreat.base.urls"))
 )
 
-if not settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    )
