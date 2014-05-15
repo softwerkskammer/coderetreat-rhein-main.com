8b69fb0ad5b569e31c244bc24da6d22407d12765
Johannes <johannes.w.seitz@gmail.com>
2014-05-15 17:52:29 +0200

    Minor template fixes

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://www.coderetreat-rhein-main.com'
+SITEURL = 'http://softwerkskammer.github.io/coderetreat-rhein-main.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
